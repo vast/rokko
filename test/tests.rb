d8a65e6e39ea3b049806a7409505e7eb0c0005a6
Vasily Polovnyov <vazilla@gmail.com>
2012-09-21 15:55:03 +0400

    bump TODO

--- test/tests.rb
+++ test/tests.rb
@@ -3,7 +3,3 @@ require File.expand_path('../helper', __FILE__)
 require File.expand_path('../../lib/rokko', __FILE__)
 
 Dir[File.expand_path('../test_*.rb', __FILE__)].each {|f| require f}
-
-# TODO rake test task
-# TODO index generating tests
-# TODO assets (css, js) tests
